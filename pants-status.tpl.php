df4ad9155f5cef5a1808d140c1c4c2f9797fb0a1
effulgentsia <alex.bronstein@acquia.com>
2013-05-16 10:42:25 -0700

    Moved pants_status theming to a template.

---
+++ pants-status.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display the user's pants status.
+ *
+ * This template is for demonstration purposes only. Please, for the love of the
+ * internet, never output a <blink> tag in a real module or website.
+ *
+ * Available variables:
+ * - $content: The pants status content. Use render($content) to print it.
+ *
+ * @ingroup themeable
+ */
+?>
+<blink><?php print render($content); ?></blink>
